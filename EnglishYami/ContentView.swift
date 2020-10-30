1fef6673da6e639173609a316c6c5975a97835cf
Алексей Селиванов <a.selivanov@digital-lab.ru>
2020-10-30 16:41:31 +0300

    Initial Commit

---
+++ EnglishYami/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnglishYami
+//
+//  Created by Алексей Селиванов on 30.10.2020.
+//  Copyright © 2020 Alexey Selivanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
